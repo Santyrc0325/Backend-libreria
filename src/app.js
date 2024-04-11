8e33febf10e68b1b8f332e1b6fe927a61e6f4fad
Santyrc0325 <scamachor@uniempresarial.edu.co>
2024-04-11 00:37:06 -0500

    Conexion a MySQL

--- src/app.js
+++ src/app.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { Sequelize } = require('sequelize');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Conexión a MongoDB
-mongoose.connect('mongodb+srv://admin:sLGZLzx59bDHj7lI@cluster0testeo.7iajvcj.mongodb.net/')
+mongoose.connect('mongodb+srv://admin:sLGZLzx59bDHj7lI@cluster0testeo.7iajvcj.mongodb.net/miBaseDeDatos?retryWrites=true&w=majority')
     .then(() => console.log('Conectado a MongoDB'))
     .catch((error) => console.error('No se pudo conectar a MongoDB', error));
 
-// Uso de rutas
+const sequelize = new Sequelize('libreria', 'andresmosquera', 'abogadoColmenares123?*', {
+    host: '35.225.222.50',
+    port: 3306,
+    dialect: 'mysql'
+});
+
+sequelize.authenticate()
+    .then(() => console.log('Conectado a MySQL'))
+    .catch((error) => console.error('No se pudo conectar a MySQL', error));
+
 const authRoutes = require('./routes/authRoutes');
 const librosRoutes = require('./routes/librosRoutes');
 app.use('/api/auth', authRoutes);
 app.use('/api/libros', librosRoutes);
 
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
-
