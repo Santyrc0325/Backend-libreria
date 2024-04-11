88cd439d686bf0d3e88d7ebf62d63ba7d3a4cea8
Santyrc0325 <scamachor@uniempresarial.edu.co>
2024-04-10 22:31:57 -0500

    error corregido

--- src/models/libro.js
+++ src/models/libro.js
@@ -5,7 +5,24 @@ const libroSchema = new mongoose.Schema({
     titulo: { type: String, required: true },
     autor: { type: String, required: true },
     editorial: { type: String, required: true },
-    categoria: { type: String, required: true },
+    categoria: [{ 
+        type: String, 
+        required: true,
+        enum: [
+            'Clásicos', 
+            'Filosofía', 
+            'Biografía', 
+            'Autoayuda', 
+            'Ciencia Ficción', 
+            'Ciencia', 
+            'Novelas', 
+            'Economía', 
+            'Arte', 
+            'Salud y bienestar', 
+            'Historia', 
+            'Negocios'
+        ] 
+    }],
     fecha: {type: String, required: true},
     idioma: { type: String, required: true },
     num_paginas: { type: Number, required: true },
